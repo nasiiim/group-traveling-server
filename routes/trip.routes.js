b6a52c7689ca684a7d7be910b7167320247a1a66
Nasim ghalebi <n.ghalebi@gmail.com>
2023-03-08 17:24:59 +0100

    Add if to get all of the trip for type(own or all)

--- routes/trip.routes.js
+++ routes/trip.routes.js
@@ -3,50 +3,62 @@ const router = express.Router();
 const mongoose = require("mongoose");
 
 const Trip = require("../models/Trip.model")
+const { isAuthenticated } = require("../middleware/jwt.middleware");
+
 
 
 //create new trip
 router.post("/trips", (req, res, next) => {
-    const { startDate, endDate, destination } = req.body
+    const { creatorId, startDate, endDate, destination } = req.body
 
-    Trip.create({ startDate, endDate, destination })
-    .then((response) => res.json(response) )
-    .catch((err) => res.json(err))
+    Trip.create({ creatorId, startDate, endDate, destination })
+        .then((response) => res.json(response))
+        .catch((err) => res.json(err))
 })
 
 //get all of the trips
-router.get("/trips", (req, res, next) => {
-    Trip.find()
-    // .populate("subscriber")
-    .then((allTrips) => res.json(allTrips))
-    .catch((err) => res.json(err))
+router.get("/trips", isAuthenticated, (req, res, next) => {
+    console.log(`req.payload`, req.payload);
+    const type = req.query.type;
+    if (type === 'own') {
+        const userId = req.payload._id
+        Trip.find({ "creatorId": userId })
+            .populate("creatorId")
+            .then((filteredTrips) => res.json(filteredTrips))
+            .catch((err) => res.json(err))
+    } else {
+        Trip.find()
+            // .populate("subscriber")
+            .then((allTrips) => res.json(allTrips))
+            .catch((err) => res.json(err))
+    }
+
 })
 
 // get a specific trip by id
 router.get("/trips/:tripId", (req, res, next) => {
     const { tripId } = req.params
-
     if (!mongoose.Types.ObjectId.isValid(tripId)) {
         res.status(400).json({ message: "Specified id is not valid" })
         return
     }
 
     Trip.findById(tripId)
-    // .populate("subscriber")
-    .then((trip) => res.status(200).json(trip))
-    .catch((err) => res.json(err))
+        // .populate("subscriber")
+        .then((trip) => res.status(200).json(trip))
+        .catch((err) => res.json(err))
 })
 
 
 
 
-router.get('/trips/:destination', (req, res)=>{
+router.get('/trips/:destination', (req, res) => {
     const destination = req.params.destination
 
-     Trip.find({ "destination" : {$regex: '.*' + destination + '.*' } })
-     .then((trip)=> res.json(trip))
-     .catch((err) => res.json(err))
-    })
+    Trip.find({ "destination": { $regex: '.*' + destination + '.*' } })
+        .then((trip) => res.json(trip))
+        .catch((err) => res.json(err))
+})
 
 
 
@@ -58,11 +70,11 @@ router.put("/trips/:tripId", (req, res, next) => {
         res.status(400).json({ message: "Specified id is not valid" })
         return
     }
-     
+
 
     Trip.findByIdAndUpdate(tripId, req.body, { new: true })
-     .then((updatedTrip) => res.json(updatedTrip))
-     .catch((err) => res.json(err))
+        .then((updatedTrip) => res.json(updatedTrip))
+        .catch((err) => res.json(err))
 })
 
 
@@ -71,13 +83,13 @@ router.put("/trips/:tripId", (req, res, next) => {
 router.delete("/trips/:tripId", (req, res, next) => {
     const { tripId } = req.params
 
-   Trip.findByIdAndRemove(tripId) 
-   .then(() =>
-   res.json({
-    message: `Trip with ${tripId} is removed successfully.`,
-   })
-   )
-   .catch((err) => res.json(err))
+    Trip.findByIdAndRemove(tripId)
+        .then(() =>
+            res.json({
+                message: `Trip with ${tripId} is removed successfully.`,
+            })
+        )
+        .catch((err) => res.json(err))
 })
 
 module.exports = router;
